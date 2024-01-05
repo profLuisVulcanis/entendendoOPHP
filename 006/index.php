0fe0f62a4dc6e4ff9400976cc2fcd3bf57708872
profLuisVulcanis <webcursoprofluis@gmail.com>
2024-01-05 19:53:57 -0300

    Aula 006 - Echo e Print
    Uso dos comandos de retorno ECHO e PRINT e suas diferenças.

---
+++ 006/index.php
@@ -0,0 +1,15 @@
+<?php
+
+//Comando echo & print
+
+echo "Retorno com Echo"; // Não possui valor de retorno
+
+//$echoReturn = echo "Dará erro";
+
+$printReturn = print "<br>Não dará Erro!";
+
+echo $printReturn;
+
+echo "<br>", "Olá", " Mundo!", " Hoje", " é um grande dia!", "<br>";
+
+print "Apenas um argumento <br>";
